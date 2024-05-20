f94e693580836e4804b66a662733281d3cd2aa6d
Daniyar Zhadyrassyn <daniyar.zhadyrassyn@agile-track.com>
2024-05-20 16:22:20 +0500

    Initial commit

---
+++ src/main/java/dzhadyrassyn/coding/challenges/Main.java
@@ -0,0 +1,8 @@
+package dzhadyrassyn.coding.challenges;
+
+public class Main {
+    public static void main(String[] args) {
+        String fileName = args[0];
+        System.out.println(fileName);
+    }
+}
