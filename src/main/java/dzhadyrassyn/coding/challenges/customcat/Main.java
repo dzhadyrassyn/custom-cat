f435ef8a7832c7cae7c9aaa1020a212e88acab79
Daniyar Zhadyrassyn <daniyar.zhadyrassyn@agile-track.com>
2024-06-06 10:46:24 +0500

    Finish step 3. Printing multiple files

--- src/main/java/dzhadyrassyn/coding/challenges/customcat/Main.java
+++ src/main/java/dzhadyrassyn/coding/challenges/customcat/Main.java
@@ -1,12 +1,12 @@
 package dzhadyrassyn.coding.challenges.customcat;
 
 import java.io.*;
+import java.nio.file.NoSuchFileException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            System.out.println("Please provide a file name to read from");
-            System.exit(0);
+            throw new NoSuchFileException("File name should be provided");
         } else if (args.length == 1 && args[0].equals("-")) {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String line;
@@ -15,11 +15,19 @@ public class Main {
             }
             br.close();
         } else {
-            File file = new File(args[0]);
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+            for (String fileName : args) {
+                File file = new File(fileName);
+                if (!file.isFile()) {
+                    throw new NoSuchFileException("Not such file: " + fileName);
+                } else if (!fileName.endsWith("txt")) {
+                    throw new WrongFileFormatExtension("File should be in txt format");
+                }
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+                br.close();
             }
         }
     }
---
+++ src/main/java/dzhadyrassyn/coding/challenges/customcat/WrongFileFormatExtension.java
@@ -0,0 +1,8 @@
+package dzhadyrassyn.coding.challenges.customcat;
+
+public class WrongFileFormatExtension extends RuntimeException {
+
+    public WrongFileFormatExtension(String message) {
+        super(message);
+    }
+}
