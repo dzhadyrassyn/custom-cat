94eacd25c08ade1523eac4bd47fdb57864c9d3ac
Daniyar Zhadyrassyn <daniyar.zhadyrassyn@agile-track.com>
2024-06-06 10:39:02 +0500

    Finish step 2. Reading from input

--- src/main/java/dzhadyrassyn/coding/challenges/customcat/Main.java
+++ src/main/java/dzhadyrassyn/coding/challenges/customcat/Main.java
@@ -1,22 +1,26 @@
 package dzhadyrassyn.coding.challenges.customcat;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
             System.out.println("Please provide a file name to read from");
             System.exit(0);
-        }
-
-        File file = new File(args[0]);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
+        } else if (args.length == 1 && args[0].equals("-")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } else {
+            File file = new File(args[0]);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
         }
     }
 }
