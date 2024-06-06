a842ba24a9d63be5241f44e3e54426b17e39d435
Daniyar Zhadyrassyn <daniyar.zhadyrassyn@agile-track.com>
2024-06-06 10:50:51 +0500

    Finish step 4. Numbering lines of input data

--- src/main/java/dzhadyrassyn/coding/challenges/customcat/Main.java
+++ src/main/java/dzhadyrassyn/coding/challenges/customcat/Main.java
@@ -1,12 +1,11 @@
 package dzhadyrassyn.coding.challenges.customcat;
 
 import java.io.*;
-import java.nio.file.NoSuchFileException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            throw new NoSuchFileException("File name should be provided");
+            throw new FileNotFoundException("File name should be provided");
         } else if (args.length == 1 && args[0].equals("-")) {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String line;
@@ -14,11 +13,19 @@ public class Main {
                 System.out.println(line);
             }
             br.close();
+        } else if (args.length == 1 && args[0].equals("-n")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            int cnt = 1;
+            while ((line = br.readLine()) != null) {
+                System.out.println((cnt++) + " " + line);
+            }
+            br.close();
         } else {
             for (String fileName : args) {
                 File file = new File(fileName);
                 if (!file.isFile()) {
-                    throw new NoSuchFileException("Not such file: " + fileName);
+                    throw new FileNotFoundException("Not such file: " + fileName);
                 } else if (!fileName.endsWith("txt")) {
                     throw new WrongFileFormatExtension("File should be in txt format");
                 }
